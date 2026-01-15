199b5307595fc6599088d43e26c66b1c9f679c27
isifitz <jadenfitz05@gmail.com>
2026-01-15 19:41:08 +0200

    Lowkey doing too much lol

---
+++ routemath.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def main():
+    routes = csv_reader()
+
+def csv_reader():
+    routes = []
+    for y in range(4):
+        x = f"./info/route{y+1}.csv"
+        with open(x,"r") as f:
+            q = pd.read_csv(f)
+            q.plot()
+            plt.show()
+    #return routes
+
+'''
+need something to take in csv
+then multiple all distances but 7
+get the total distances
+
+'''
+
+def export_info(route,values):
+    ...
+    with open(route,"w") as f:
+        for value in values:
+            ...
+
+
+if __name__ == "__main__":
+    #main()
+    csv_reader()
